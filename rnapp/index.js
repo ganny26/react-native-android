d0a3fe31f078d25984143d7f00ebc0e4ba739c3f
Selvaganesh <selvaganeshrajam@gmail.com>
2019-03-13 16:06:20 +0530

    index file changes

--- rnapp/index.js
+++ rnapp/index.js
@@ -1,12 +1,27 @@
 import React from 'react';
-import {AppRegistry, StyleSheet, Text, View} from 'react-native';
+import {AppRegistry, StyleSheet, Text, View,Button} from 'react-native';
 
 class HelloWorld extends React.Component {
+
+  state = {
+    message:'Connected to Server'
+  }
+
+  handleClick = (e)=>{
+    console.log('clicked');
+    this.setState({
+      message:'Handle Updated'
+    })
+  }
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.hello}>Hello, World From React Native</Text>
-        <Text style={styles.hello}>Connected to Server</Text>
+        <Text style={styles.hello}>{this.state.message}</Text>
+        <View style={styles.buttonPrimary}>
+        <Button  title="Reload" onPress={this.handleClick}/>
+        </View>
+
       </View>
     );
   }
@@ -23,6 +38,11 @@ var styles = StyleSheet.create({
     textAlign: 'center',
     margin: 10,
   },
+  buttonPrimary:{
+    alignItems:'center',
+    justifyContent:'center',
+    // width:150
+  }
 });
 
 AppRegistry.registerComponent('MyReactNativeApp', () => HelloWorld);
